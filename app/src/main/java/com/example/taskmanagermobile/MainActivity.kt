e75d30e435caa7899fafde96215d1c09145ea9fc
MatheusKlenk <matheus.klenklima@icloud.com>
2026-09-04 13:56:45 -0300

    feat: add Login page UI

--- app/src/main/java/com/example/taskmanagermobile/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagermobile/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App(modifier: Modifier = Modifier) {
-    val page = "HomePage"
+    val page = "Login"
 
     when (page) {
         "Login" -> Login(modifier = modifier)
